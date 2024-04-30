f6a52de5424d5c6edd6f30953327c3fa6c0fd9ae
Ramazan Arıcı <ramazanarc5@gmail.com>
2024-04-30 14:24:16 +0300

    Creating an API Endpoint

--- actions/createPostAction.ts
+++ actions/createPostAction.ts
@@ -1,5 +1,8 @@
 "use server";
 
+import { AddPostRequestBody } from "@/app/api/posts/route";
+import { Post } from "@/mongodb/models/post";
+import { IUser } from "@/types/user";
 import { currentUser } from "@clerk/nextjs/server";
 
 export default async function createPostAction(formData: FormData) {
@@ -18,9 +21,36 @@ export default async function createPostAction(formData: FormData) {
 
   // define user
 
+  const userDB: IUser = {
+    userId: user.id,
+    userImage: user.imageUrl,
+    firstName: user.firstName || "",
+    lastName: user.lastName || "",
+  };
+
   // upload image if there is one
 
-  // create post in database
+  try {
+    if (image.size > 0) {
+      // 1. upload image if there is one
+      // 2. create post in database
+      const body: AddPostRequestBody = {
+        user: userDB,
+        text: postInput,
+        //   imageUrl: image_url
+      };
+    } else {
+      // 1. create post in database without image
+      const body: AddPostRequestBody = {
+        user: userDB,
+        text: postInput,
+      };
+
+      await Post.create(body);
+    }
+  } catch (error: any) {
+    throw new Error("Failed to create post", error);
+  }
 
   // revalidatePath "/" -home page
 }
